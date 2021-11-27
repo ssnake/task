9f5818574e91f64e27e4959964a051bbe82ffa79
snake <max.nedelchev@gmail.com>
2021-11-27 11:27:36 +0200

    added test to cover new scenarios

--- api/stocks.rb
+++ api/stocks.rb
@@ -2,23 +2,38 @@ require_relative "ping"
 
 module Stocks
   class API < Grape::API
+    include Grape::Extensions::Hashie::Mash::ParamBuilder
     version 'v1'
     prefix :api
+    
     content_type :jsonapi, "application/vnd.api+json"
+    content_type :json, 'application/json'
+    
     formatter :json, Grape::Formatter::Jsonapi
     formatter :jsonapi, Grape::Formatter::Jsonapi
-    default_format :jsonapi
-
     
+    default_format :jsonapi
+    
+    params do
+      optional :stock, type: Hash do
+        optional :name, type: String
+        optional :bearer_name, type: String
+      end
+    end
+
     namespace :stocks do
-      
+
       get :index do
         stocks = Stock.all
         render stocks# include: [:bearer]
       end
 
       post :create do
-
+        bearer = Bearer.find_or_create_by name: params.stock.bearer_name
+        stock = Stock.find_or_create_by name: params.stock.name
+        BearerStockAssociation.find_or_create_by bearer: bearer, stock: stock
+        render stock, include: [:bearers]
+        
       end
     end
   end
--- spec/api/task_spec.rb
+++ spec/api/task_spec.rb
@@ -37,6 +37,55 @@ RSpec.describe Task::API do
         expect(JSON.parse(last_response.body)).to eq({})
       end 
     end
+
+    describe "#create" do
+       let(:body) {
+          {stock: {name: name, bearer_name: bearer_name}}
+        }
+
+      context "with new stock and new bearer names" do
+        let(:name) { "new_stock"}
+        let(:bearer_name) {"new bearer"}
+
+        it "creates new stock and new bearer" do
+          expect do
+            post '/api/v1/stocks/create', body.to_json, 'CONTENT_TYPE' => 'application/json'
+            expect(last_response.status).to eq(201)
+          end.to change { Stock.count }
+            .and change { Bearer.count }
+            .and change {BearerStockAssociation.count }
+           
+        end
+      end
+
+      context "with existed stock and bearer name" do
+        let(:name) { stocks[1][:name]}
+        let(:bearer_name) { bearer2[:name]}
+        it "doesn't add new record to stock and bearer, but 1 to bearer stock association" do
+          expect do
+            post '/api/v1/stocks/create', body.to_json, 'CONTENT_TYPE' => 'application/json'
+            expect(last_response.status).to eq(201)
+          end.to change { Stock.count }.by(0)
+            .and change { Bearer.count }.by(0)
+            .and change {BearerStockAssociation.count }
+        end
+        
+        context "with existed association" do
+          let(:name) { stocks[0][:name]}
+          let(:bearer_name) { bearer1[:name]}
+
+          it "doesn't add any new record" do
+            expect do
+              post '/api/v1/stocks/create', body.to_json, 'CONTENT_TYPE' => 'application/json'
+              expect(last_response.status).to eq(201)
+            end.to change { Stock.count }.by(0)
+              .and change { Bearer.count }.by(0)
+              .and change {BearerStockAssociation.count }.by(0)
+          end
+        end
+      end
+        
+    end
   end
 
 end
